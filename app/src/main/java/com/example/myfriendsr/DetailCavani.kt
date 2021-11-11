02be8a7a82479907d6b56d9fc563617cf3ae4578
Edherrera <edherrera96@uan.edu.co>
2021-11-10 23:12:01 -0500

    se agrego las calificaciones a todos los details

--- app/src/main/java/com/example/myfriendsr/DetailCavani.kt
+++ app/src/main/java/com/example/myfriendsr/DetailCavani.kt
@@ -2,10 +2,19 @@ package com.example.myfriendsr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.Toast
 
 class DetailCavani : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_cavani)
+
+        val ratingforapp = findViewById<RatingBar>(R.id.ratingBar5)
+        //val textViewPuntaje = findViewById<TextView>(R.id.textViewPuntaje)
+        ratingforapp.setOnRatingBarChangeListener { ratinBar, fl, b ->
+            //textViewPuntaje.text = fl.toString()
+            Toast.makeText(this, "Calificaste: ${fl}!!", Toast.LENGTH_LONG).show()
+        }
     }
 }
