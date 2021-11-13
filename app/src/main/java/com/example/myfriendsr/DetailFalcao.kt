ed7503dbe249117f8924ac59631571582d9a9877
Edherrera <edherrera96@uan.edu.co>
2021-11-13 08:35:07 -0500

    se alineo la actividad principal

--- app/src/main/java/com/example/myfriendsr/DetailFalcao.kt
+++ app/src/main/java/com/example/myfriendsr/DetailFalcao.kt
@@ -13,7 +13,7 @@ class DetailFalcao : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_falcao)
-        val ratingforapp = findViewById<RatingBar>(R.id.ratingBar4)
+        val ratingforapp = findViewById<RatingBar>(R.id.ratingBarFalcao)
         val textViewPuntaje = findViewById<TextView>(R.id.textViewPuntaje)
         ratingforapp.setOnRatingBarChangeListener { ratinBar, fl, b ->
             textViewPuntaje.text = fl.toString()
